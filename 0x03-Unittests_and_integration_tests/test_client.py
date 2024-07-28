45251fc8271a1b8e0223596305d951901590b3a7
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-28 18:49:43 +0000

    Client module Test Suite: test for GithubOrgClient.org()

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Client modules test suite
+"""
+
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch
+client = __import__('client').GithubOrgClient
+
+
+class TestGithhubOrgClient(unittest.TestCase):
+    """
+        Class test suite of client
+    """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+        ])
+    @patch("client.get_json")
+    def test_org(self, param, mock_json):
+        """test definition"""
+        cli = client(param)
+        mock_json.return_value = {"payload": True}
+        self.assertEqual(cli.org, {"payload": True})
